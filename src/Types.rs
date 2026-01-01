a4426d30419cb08fe197de7811d800f9172c866e
hanbeul <han310@gmail.com>
2026-01-01 12:56:46 -0500

    Convert transactions from multiple types into a single list of transactions
    Initial implem

--- src/Types.rs
+++ src/Types.rs
@@ -1,26 +1,57 @@
 use chrono::prelude::*;
 use rusty_money::{Money, iso};
+use serde::{Serialize, Deserialize};
 
+// Generic Transaction
 #[derive(Debug)]
 pub struct Transaction<'a> {
   pub date: chrono::NaiveDate,
   pub name: String,
   pub amount: rusty_money::Money<'a, iso::Currency>,
+  pub source: String,
 }
 
 #[derive(Debug)]
-pub struct CapOneTransaction {
-  pub date: String,
+pub struct VentureXTransaction {
+  pub transaction_date: String,
+  pub posted_date: String,
+  pub card_no: f64,
   pub description: String,
-  pub amount: String,
+  pub category: String,
+  pub debit: f64,
+  pub credit: f64,
 }
 
-impl From<CapOneTransaction> for Transaction<'_>{
-  fn from(t: CapOneTransaction) -> Self {
-    Transaction {
-      date: NaiveDate::parse_from_str(&t.date, "%m/%d/%Y").unwrap(),
-      name: t.description,
-      amount: Money::from_str(&t.amount, iso::USD).unwrap(),
-    }
-  }
-}
+//impl From<VentureXTransaction> for Transaction<'_>{
+//  fn from(t: VentureXTransaction) -> Self {
+//    Transaction {
+//      date: NaiveDate::parse_from_str(&t.tran, "%Y-%m-%d").unwrap(),
+//      name: t.description,
+//      amount: Money::from_str(&t.amount, iso::USD).unwrap(),
+//    }
+//  }
+//}
+
+//#[derive(Debug)]
+//pub struct SapphireTransaction {
+//  pub account_number: i64,
+//  pub transaction_description: String,
+//  pub transaction_date: String,
+//  pub transaction_type:String,
+//  pub transaction_amount:String,
+//  pub balance:String,
+//
+//  pub date: String,
+//  pub description: String,
+//  pub amount: String,
+//}
+//
+//impl From<SapphireTransaction> for Transaction<'_>{
+//  fn from(t: SapphireTransaction) -> Self {
+//    Transaction {
+//      date: NaiveDate::parse_from_str(&t.date, "%m/%d/%Y").unwrap(),
+//      name: t.description,
+//      amount: Money::from_str(&t.amount, iso::USD).unwrap(),
+//    }
+//  }
+//}
--- src/main.rs
+++ src/main.rs
@@ -8,77 +8,166 @@ use chrono::prelude::*;
 
 mod Types;
 use crate::Types::Transaction;
-use crate::Types::CapOneTransaction;
+use crate::Types::VentureXTransaction;
 
-fn convert() -> Result<(), Box<dyn Error>> {
-  let c1 = CapOneTransaction {
-    date: String::from("05/30/1992"),
-    description: String::from("This is a transaction description"),
-    amount: String::from("99.99"),
-  };
+//fn convert() -> Result<(), Box<dyn Error>> {
+//  let c1 = CapOneTransaction {
+//    date: String::from("05/30/1992"),
+//    description: String::from("This is a transaction description"),
+//    amount: String::from("99.99"),
+//  };
+//
+//  let t: Transaction = c1.into();
+//  println!("The converted transaction is: {:?}!", t);
+//  Ok(())
+//}
 
-  let t: Transaction = c1.into();
-  println!("The converted transaction is: {:?}!", t);
-  Ok(())
+fn print_type<T>(_: &T) {
+    println!("Type: {}", std::any::type_name::<T>());
 }
 
-fn parse_transactions<'a, T>(mut rdr: csv::Reader<T>) -> Result<Vec<Transaction<'a>>, Box<dyn Error>> where T: std::io::Read {
+fn parse_transactions<'a, T>(rdr: &mut csv::Reader<T>) -> Result<Vec<Transaction<'a>>, Box<dyn Error>> where T: std::io::Read {
   let mut transactions: Vec<Transaction> = vec![];
-  for result in rdr.records() {
-    // The iterator yields Result<StringRecord, Error>, so we check the
-    // error here.
-    let record = result?;
+  let mut source: &str = "";
 
-    let t = Transaction {
-      date: NaiveDate::parse_from_str(&record[0], "%m/%d/%Y")?,
-      name: record[2].to_string(),
-      amount: Money::from_str(&record[5], iso::USD)?,
-    };
-    println!("{:?}", t);
-    transactions.push(t);
+  // Read the first record (headers)
+  // Determine what type of csv based off headers
+  let headers = rdr.headers()?;
+  let header_vec: Vec<String> = headers
+    .iter()
+    .map(|header| header.to_string())
+    .collect();
+  println!("{:?}", headers);
+  if headers.iter().any(|field| field == "Memo") {
+    source = "chase";
+  } else if headers.iter().any(|field| field == "Account Number") {
+    source = "checking";
+  } else if headers.iter().any(|field| field == "Card No.") {
+    source = "venture";
+  }
+
+  println!("source: {:?}", source);
+
+  if source == "chase" {
+    for result in rdr.records() {
+      // The iterator yields Result<StringRecord, Error>, so we check the
+      // error here.
+      let record = result?;
+
+      let t = Transaction {
+        date: NaiveDate::parse_from_str(&record[0], "%m/%d/%Y")?,
+        name: record[2].to_string(),
+        amount: Money::from_str(&record[5], iso::USD)?,
+        source: source.to_string(),
+      };
+
+      println!("{:?}", t);
+      transactions.push(t);
+    }
+  } else if source == "venture" {
+    for result in rdr.records() {
+      // The iterator yields Result<StringRecord, Error>, so we check the
+      // error here.
+      let record = result?;
+
+      let mut amount: Money<iso::Currency>;
+      if &record[5] != "" {
+        amount = Money::from_str(&record[5], iso::USD)? * -1;
+      } else {
+        amount = Money::from_str(&record[6], iso::USD)?;
+      }
+
+      let t = Transaction {
+        date: NaiveDate::parse_from_str(&record[0], "%Y-%m-%d")?,
+        name: record[3].to_string(),
+        //amount: Money::from_str(&record[5], iso::USD)?,
+        amount,
+        source: source.to_string(),
+      };
+      println!("{:?}", t);
+      transactions.push(t);
+    }
+  } else if source == "checking" {
+    for result in rdr.records() {
+      // The iterator yields Result<StringRecord, Error>, so we check the
+      // error here.
+      let record = result?;
+
+      let transaction_type = &record[3];
+
+      let mut amount: Money<iso::Currency>;
+      if transaction_type == "Debit" {
+        amount = Money::from_str(&record[4], iso::USD)? * -1;
+      } else {
+        amount = Money::from_str(&record[4], iso::USD)?;
+      }
+
+      let t = Transaction {
+        date: NaiveDate::parse_from_str(&record[2], "%m/%d/%Y")?,
+        name: record[1].to_string(),
+        amount,
+        source: source.to_string(),
+      };
+      println!("{:?}", t);
+      transactions.push(t);
+    }
+  } else {
   }
 
   transactions.sort_by(|a, b| {
-      if b.amount > a.amount {
-          Ordering::Less
-      } else if b.amount < a.amount {
-          Ordering::Greater
-      } else {
-          Ordering::Equal
-      }
+    if b.amount > a.amount {
+      Ordering::Less
+    } else if b.amount < a.amount {
+      Ordering::Greater
+    } else {
+      Ordering::Equal
+    }
   });
   Ok(transactions)
 }
 
 fn read_from_file() -> Result<(), Box<dyn Error>> {
+  let mut transactions: Vec<Transaction> = vec![];
+
   // Read files in current dir? 
-  for entry in std::fs::read_dir(".")? {
+  for entry in std::fs::read_dir("data")? {
     let entry = entry?;
     let path = entry.path();
-    if !path.is_dir() {
-      println!("{:?}", path.extension());
+    if let Some(extension) = path.extension() {
+      if extension == "csv" && !path.is_dir(){
+        println!("{:?}", path.file_name().unwrap());
+
+        let file = std::fs::File::open(&path)?;
+        let mut rdr = csv::Reader::from_reader(file);
+
+        transactions.extend(parse_transactions(&mut rdr)?);
+      }
     }
   }
-
-  let file = std::fs::File::open("chase.csv")?;
-  let rdr = csv::Reader::from_reader(file);
-
-  let transactions = parse_transactions(rdr);
   println!("{:?}", transactions);
+
+  //let file = std::fs::File::open("data/2025-12-30_Checking...4181.csv")?;
+  //let mut rdr = csv::Reader::from_reader(file);
+
+  //let transactions = parse_transactions(&mut rdr);
+  //println!("{:?}", transactions);
   Ok(())
 }
 
 fn read_from_stdin() -> Result<(), Box<dyn Error>> {
   // Build the CSV reader and iterate over each record.
-  let rdr = csv::Reader::from_reader(io::stdin());
+  let mut rdr = csv::Reader::from_reader(io::stdin());
 
-  let transactions = parse_transactions(rdr);
+  let transactions = parse_transactions(&mut rdr);
   println!("{:?}", transactions);
   Ok(())
 }
 
+fn write_output(transactions: Vec<Transaction>) {
+}
+
 fn main() {
-  if let Err(err) = read_from_stdin() {
+  if let Err(err) = read_from_file() {
     println!("error running test: {}", err);
     process::exit(1);
   }
